28e34469772ca84b8ea67327bcdce8df33c06786
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-23 12:19:36 +0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { useState } from "react";
+import AllStarships from "./Pages/AllStarships";
+import SingleStarship from "./Pages/SingleStarship";
 
 function App() {
+    const [selectedStarship, setSelectedStarship] = useState({});
 
     return (
-        <>
-            
-        </>
+        <div className="main-area">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<AllStarships setSelectedStarship={setSelectedStarship} />} />
+                    <Route path={`/${selectedStarship.name}`} element={<SingleStarship selectedStarship={selectedStarship} />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 }
 
