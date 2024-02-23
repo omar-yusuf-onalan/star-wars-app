10aeae45a68b9c2a6aa747da9da8bebfd9639f56
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-23 13:50:36 +0300

    Update Starships.jsx

--- src/Components/Starships/Starships.jsx
+++ src/Components/Starships/Starships.jsx
@@ -1,24 +1,65 @@
 import { useEffect, useState } from "react";
-import { getStarships } from "../../API/Starship";
+import { getStarships, filterStarships } from "../../API/Starship";
 import "./Starships.style.css";
-import Starship from "../Starship/Starship";
+import StarshipWithLessDetail from "../StarshipWithLessDetail.jsx/StarshipWithLessDetail";
 
 const Starships = ({ setSelectedStarship }) => {
     const [starships, setStarships] = useState([]);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
-        getStarships().then((data) => setStarships(data.results));
-    }, []);
+        getStarships(page).then((data) => setStarships(data.results));
+    }, [page]);
 
+    const handleClick = (starship) => {
+        setSelectedStarship(starship);
+    };
+
+    const handleFilter = () => {
+        const userInput = document.querySelector("#name-model-input").value;
+
+        filterStarships(userInput).then((data) => setStarships(data.results));
+    };
     return (
-        <div className="starships">
-            {starships.map((starship) => {
-                return (
-                    <Starship key={starship.name} selectedStarship={starship} />
-                );
-            })}
-            ;
-        </div>
+        <>
+            <div>
+                <span>Name / Model</span>
+                <input
+                    id="name-model-input"
+                    type="text"
+                    placeholder="Name / Model"
+                />
+                <button onClick={handleFilter}> Filter Starships</button>
+            </div>
+
+            <div className="starships">
+                {starships.map((starship) => {
+                    return (
+                        <StarshipWithLessDetail
+                            key={starship.name}
+                            selectedStarship={starship}
+                            setSelectedStarship={setSelectedStarship}
+                            onClick={handleClick(starship)}
+                        />
+                    );
+                })}
+                ;
+            </div>
+            <button
+                onClick={() => {
+                    setPage((prev) => prev - 1);
+                }}
+            >
+                See previous page
+            </button>
+            <button
+                onClick={() => {
+                    setPage((prev) => prev + 1);
+                }}
+            >
+                See next page
+            </button>
+        </>
     );
 };
 
