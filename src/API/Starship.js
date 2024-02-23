cc562624486ff5ec88a200ed5c44395245e89176
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-23 13:50:10 +0300

    Add filterStarships method

--- src/API/Starship.js
+++ src/API/Starship.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-export const getStarships = async () => {
-    const { data } = await axios.get("https://swapi.dev/api/starships/");
+export const getStarships = async (pageNumber) => {
+    const { data } = await axios.get(`https://swapi.dev/api/starships/?page=${pageNumber}`);
+
+    return data;
+};
+
+export const filterStarships = async (query) => {
+    const { data } = await axios.get(`https://swapi.dev/api/starships/?search=${query}`);
 
     return data;
 };
