cee9430bd1f16bc68b2888551df98344bfec8ce5
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-23 12:20:01 +0300

    Create Starship.jsx

---
+++ src/Components/Starship/Starship.jsx
@@ -0,0 +1,21 @@
+import "./Starship.style.css";
+
+const Starship = ({ selectedStarship }) => {
+    return (
+        <div className="single-starship">
+            <h1>{selectedStarship.name}</h1>
+            <div>Model: {selectedStarship.model}</div>
+            <div>Hyperdrive Rating: {selectedStarship.hyperdrive_rating}</div>
+            <div>Passengers: {selectedStarship.passengers}</div>
+            <div>
+                Max Atmosphering Speed:{" "}
+                {selectedStarship.max_atmosphering_speed}{" "}
+            </div>
+            <div>Manufacturer: {selectedStarship.manufacturer} </div>
+            <div>Crew: {selectedStarship.crew} </div>
+            <div>Cargo Capacity: {selectedStarship.cargo_capacity}</div>
+        </div>
+    );
+};
+
+export default Starship;
