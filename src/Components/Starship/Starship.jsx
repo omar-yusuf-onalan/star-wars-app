0d0394030a579bf863201a74408f1259e7dad73c
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-23 13:50:38 +0300

    Update Starship.jsx

--- src/Components/Starship/Starship.jsx
+++ src/Components/Starship/Starship.jsx
@@ -1,20 +1,26 @@
+import { Link } from "react-router-dom";
 import "./Starship.style.css";
 
-const Starship = ({ selectedStarship }) => {
+const Starship = ({ selectedStarship, setSelectedStarship }) => {
     return (
-        <div className="single-starship">
-            <h1>{selectedStarship.name}</h1>
-            <div>Model: {selectedStarship.model}</div>
-            <div>Hyperdrive Rating: {selectedStarship.hyperdrive_rating}</div>
-            <div>Passengers: {selectedStarship.passengers}</div>
-            <div>
-                Max Atmosphering Speed:{" "}
-                {selectedStarship.max_atmosphering_speed}{" "}
+        <>
+            <Link to={"/"}>Return to main page</Link>
+            <div className="single-starship">
+                <h1>{selectedStarship.name}</h1>
+                <div>Model: {selectedStarship.model}</div>
+                <div>
+                    Hyperdrive Rating: {selectedStarship.hyperdrive_rating}
+                </div>
+                <div>Passengers: {selectedStarship.passengers}</div>
+                <div>
+                    Max Atmosphering Speed:{" "}
+                    {selectedStarship.max_atmosphering_speed}{" "}
+                </div>
+                <div>Manufacturer: {selectedStarship.manufacturer} </div>
+                <div>Crew: {selectedStarship.crew} </div>
+                <div>Cargo Capacity: {selectedStarship.cargo_capacity}</div>
             </div>
-            <div>Manufacturer: {selectedStarship.manufacturer} </div>
-            <div>Crew: {selectedStarship.crew} </div>
-            <div>Cargo Capacity: {selectedStarship.cargo_capacity}</div>
-        </div>
+        </>
     );
 };
 
